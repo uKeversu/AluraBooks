3a29af1513b6400609783cb804bdf2e6d2653270
uKeversu <109996005+uKeversu@users.noreply.github.com>
2024-02-01 14:08:41 -0300

    Aula 5.1
    Corrigir e tratar erros.

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -7,7 +7,6 @@ function exibirOsLivrosNaTela(listaDeLivros) {
   elementoComValorTotalDeLivrosDisponiveis.innerHTML = "";
   elementoParaInserirLivros.innerHTML = "";
   listaDeLivros.forEach((livro) => {
-    // let disponibilidade = verificarDisponibilidadeDoLivro(livro)
     let disponibilidade =
       livro.quantidade > 0 ? "livro__imagens" : "livro__imagens indisponivel";
     elementoParaInserirLivros.innerHTML += `
@@ -26,11 +25,3 @@ function exibirOsLivrosNaTela(listaDeLivros) {
         `;
   });
 }
-
-// function verificarDisponibilidadeDoLivro(livro) {
-//   if (livro.quantidade > 0 ){
-//     return 'livro__imagens'
-//   } else {
-//     return 'livros__imagens indisponivel'
-//   }
-// }
